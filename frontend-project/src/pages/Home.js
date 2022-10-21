6fc933b9615558878ab7e1b8e12e63a93aedffd8
Oscar Hero <thescer6@gmail.com>
2022-10-21 15:12:53 +0300

    commit issue sucks

--- frontend-project/src/pages/Home.js
+++ frontend-project/src/pages/Home.js
@@ -4,30 +4,22 @@ import { Backdrop } from "../components/Backdrop";
 import { useState, useEffect } from "react";
 
 export const Home = () => {
-
-  const [id, setID] = useState([]);
-
-  const fetchID = async() => {
-    const response = await fetch(
-      ""
-    )
-  }
   const [news, setNews] = useState([]);
 
-  const fetchNews = async() => {
+  const fetchNews = async () => {
     const response = await fetch(
-      "https://news-project-fsd-default-rtdb.europe-west1.firebasedatabase.app/news.json",
+      "https://news-project-fsd-default-rtdb.europe-west1.firebasedatabase.app/news.json"
     );
     const data = await response.json();
 
     const fetchedNews = [];
 
-    for (const key in data){
+    for (const key in data) {
       fetchedNews.push({
         id: key.id,
         by: data[key].by,
         title: data[key].title,
-        url: data[key].url
+        url: data[key].url,
       });
     }
     setNews(fetchedNews);
@@ -36,7 +28,6 @@ export const Home = () => {
   useEffect(() => {
     fetchNews();
   }, []);
-  
 
   const [showModal, setShowModal] = useState(false);
 
