1db131d764ee2ff5a8ca7113128efce232524a6a
Oscar Hero <thescer6@gmail.com>
2022-10-21 10:08:03 +0300

    initialized each component and page and renamed screens to pages

---
+++ frontend-project/src/components/NewsItem.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NewsItem = () => {
+  return <div>NewsItem</div>;
+};
---
+++ frontend-project/src/pages/NewsScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NewsScreen = () => {
+  return <div>NewsScreen</div>;
+};
