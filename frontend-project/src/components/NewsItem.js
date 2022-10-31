141083bf2ca6b474f6a23eec13ee410e5114206a
Cabba4 <anmol.arora@tuni.fi>
2022-10-31 15:48:36 +0200

    fetch for popular and recent added

--- frontend-project/src/components/NewsItem.js
+++ frontend-project/src/components/NewsItem.js
@@ -9,7 +9,7 @@ export const NewsItem = ({ id, by, time, title, url }) => {
         <h3 className="title">Title: {title}</h3>
         <h3 className="description">Author: {by}</h3>
         <h3 className="date">Time: {time}</h3>
-        <Link to={url}>Link: {url}</Link>
+        <a target="_blank" href={url}>Link: {url}</a>
       </div>
     </div>
   );
--- frontend-project/src/pages/Popular.js
+++ frontend-project/src/pages/Popular.js
@@ -1,11 +1,97 @@
-import React from "react";
 import { NewsList } from "../components/NewsList";
+import {useState, useEffect, React} from "react";
 
 export const Popular = () => {
+
+  const fetchID = async() => {
+
+    const del = await fetch("https://news-project-fsd-default-rtdb.europe-west1.firebasedatabase.app/newsid.json")
+    const check = await del.json();
+    if (check){
+      const reset = await fetch("https://news-project-fsd-default-rtdb.europe-west1.firebasedatabase.app/newsid.json",
+      {
+        method: "DELETE"
+      })
+    } 
+    const response = await fetch(
+      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+    )
+    const data = await response.json();
+    const fetchedID = [];
+   //console.log(data);
+    for (const key in data){
+      fetchedID.push({
+        id: data[key]
+      });
+    }
+    setNewsID(fetchedID);
+  }
+  
+
+  const setNewsID = async(id) => {
+    const response = await fetch(
+      "https://news-project-fsd-default-rtdb.europe-west1.firebasedatabase.app/newsid.json",
+      {
+        method: "POST",
+        body: JSON.stringify(id),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    //const data = await response.json();
+    //console.log(data);
+    fetchNewsID();
+  }
+
+  const [news, setNews] = useState([]);
+
+  const fetchNewsID = async () => {
+
+
+    const response = await fetch(
+      "https://news-project-fsd-default-rtdb.europe-west1.firebasedatabase.app/newsid.json"
+    );
+    const data = await response.json();
+    //console.log(data);
+
+    const fetchedNews = [];
+
+    for (const key in data) {
+      const subdata = data[key];
+      if (fetchedNews.length < 20) {
+        for (var i=0; i<20; i++){
+          fetchedNews.push(subdata[i])
+        }
+      }      
+    }
+    fetchNewsBody(fetchedNews);
+  };
+
+  const finalNews = [];
+
+  const fetchNewsBody = async(fetchedNews) => {
+    // now have to send fetch request using fetchedNews id and the content should go to some component
+    for (var key in fetchedNews) {
+      //console.log("i am fetching " , fetchedNews[key].id);
+      const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${fetchedNews[key].id}.json?print=pretty`)
+      const data = await response.json();
+      //console.log(data)
+      finalNews.push(data);
+    }
+    setNews(finalNews);
+  }
+
+  useEffect(() => {
+    fetchID();
+  }, []);
+
+
+
   return (
     <div>
       <h2>Popular</h2>
-      <NewsList></NewsList>
+      <NewsList  news={news} ></NewsList>
     </div>
   );
 };
